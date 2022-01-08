65d8b0aed6e1d0cd7f9845d65d52667d2671be15
JaeWanJang <newmy526@naver.com>
2022-01-08 14:37:34 +0900

    메인 페이지 정상 작동 확인

--- switch-pro/src/main/java/com/web/home/CoverController.java
+++ switch-pro/src/main/java/com/web/home/CoverController.java
@@ -12,7 +12,15 @@ public class CoverController {
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String cover() {
 		
-		logger.info("다시 나옴");
+		logger.info("cover 요청 성공");
 		return "./cover";
 	}
+	
+	@RequestMapping(value="/main", method = RequestMethod.GET)
+	public String main() {
+		logger.info("main 페이지 정상 작동");
+		return "/jsp/main/main";
+	}
+	
+	
 }
